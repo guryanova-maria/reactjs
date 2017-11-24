c3906fe51d337d2017720ffbe1f99f25cc0746f9
Maria Guryanova <guryanova.maria@gmail.com>
2017-11-24 22:52:05 +0200

    delete items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Sidebar from './Sidebar'
 import './App.css';
 import './Sidebar.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>TO-DO List</h2>
-        </div>
-        <Sidebar />
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>TO-DO List</h2>
+                </div>
+                <Sidebar/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Categories.js
+++ src/Categories.js
@@ -8,12 +8,23 @@ class Categories extends Component {
             list: [
                 {id: 'item_01', name: 'Homework 1', nodes: [], parentId: null},
                 {id: 'item_02', name: 'Homework 2', nodes: ['item_03', 'item_04'], parentId: null},
-                {id: 'item_03', name: 'Homework 2-1', nodes: ['item_04'], parentId: 'item_02'},
-                {id: 'item_04', name: 'Homework 2-2', nodes: [], parentId: 'item_02'},
-                {id: 'item_05', name: 'Homework 2-1-2', nodes: [], parentId: 'item_03'}
-            ],
-            itemsRendered: []
+                {id: 'item_03', name: 'Homework 2-1', nodes: ['item_05', 'item_06'], parentId: 'item_02'},
+                {id: 'item_04', name: 'Homework 2-2', nodes: ['item_07'], parentId: 'item_02'},
+                {id: 'item_05', name: 'Homework 2-1-1', nodes: [], parentId: 'item_03'},
+                {id: 'item_06', name: 'Homework 2-1-2', nodes: [], parentId: 'item_03'},
+                {id: 'item_07', name: 'Homework 2-2-1', nodes: [], parentId: 'item_04'}
+            ]
         };
+
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    deleteItem(id) {
+        const newState = this.state.list.slice();
+        newState.splice(id, 1);
+        this.setState({
+            list: newState
+        });
     }
 
     render() {
@@ -21,18 +32,21 @@ class Categories extends Component {
 
         return (
             <div>
-                <List list={list} fullList={list}/>
+                <List list={list} fullList={list} parentId={null} deleteItem={this.deleteItem} />
             </div>
         );
     }
 }
 
-const List = ({list, fullList}) => {
+const List = ({list, fullList, parentId = null, deleteItem}) => {
     return(
         <ol>
             {
+
                 list.map(item =>
-                    <ListItem item={item} fullList={fullList}/>
+                    item.parentId === parentId
+                        ? <ListItem item={item} fullList={fullList} deleteItem={deleteItem}/>
+                        : null
                 )
             }
         </ol>
@@ -45,18 +59,19 @@ let getItemById = (id, fullList) => {
             return fullList[i];
         }
     }
-}
+};
 
-const ListItem = ({item, fullList}) => {
+const ListItem = ({item, fullList, deleteItem}) => {
     let nodesList = item.nodes.map(node => getItemById(node, fullList));
     return (
         <li key={item.id}>
             {item.name}
+            <button onClick={ () => {deleteItem(item.id)}}>x</button>
             {!!nodesList.length &&
-            <List list={nodesList} fullList={fullList}/>
+            <List list={nodesList} fullList={fullList} parentId={item.id} deleteItem={deleteItem}/>
             }
         </li>
     )
-}
+};
 
 export default Categories;
